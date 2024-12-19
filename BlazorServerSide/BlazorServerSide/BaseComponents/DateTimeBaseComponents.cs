97767cf0c58565c52a6809d36d67722f3971bc66
Pankaj Rajput <pankaj.rajput@softude.com>
2024-12-19 19:04:55 +0530

    fist commit

---
+++ BlazorServerSide/BlazorServerSide/BaseComponents/DateTimeBaseComponents.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorServerSide.BaseComponents
+{
+  public class DateTimeBaseComponents : ComponentBase
+  {
+    public DateTime dateTime { get; set; }
+  }
+}
